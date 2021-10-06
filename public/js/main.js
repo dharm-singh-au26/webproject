eb19269b9f4c2adfa9170a09ae6989d449626e55
DHARM SINGH <dharmjat45@gmail.com>
2021-10-06 09:05:14 +0530

    node_js_projetc

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+const cityName = document.getElementById("cityName");
+const submitBtn = document.getElementById("submitBtn");
+const city_name = document.getElementById("city_name");
+const temp_status = document.getElementById("temp_status");
+const temp_real_val = document.getElementById("temp_real_val");
+// const datahide = document.querySelector('middle_layer')
+
+const getInfo = async (event) => {
+  event.preventDefault();
+  let cityVal = cityName.value;
+
+  if (cityVal === "") {
+    city_name.innerText = `please write the city  name before search`;
+    datahide.classList.add("data_hide");
+  } else {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=e9e995d156fa3e000800e7c646cae212`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      const arrData = [data];
+      city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
+      temp_real_val.innertext = arrData[0].main.temp;
+      temp_status.innertext = arrData[0].weather[0].main;
+      /*-----------Condition to check sunny or cloudy */
+      const tempMood = arrData[0].weather[0].main;
+      if (tempMood == "Clear") {
+        temp_status.innerHTML =
+          "<i class='fas fa-sun' style='color:#eccc68'></i>";
+      } else if (tempMood == "Clouds") {
+        temp_status.innerHTML =
+          "<i>class='fas fa-cloud' style='color:#f1f2f6;'</i>";
+      } else if (tempMood == "Rain") {
+        temp_status.innerHTML =
+          "<i>class='fas fa-rain' style='color:#a4b0be;'</i>";
+      } else {
+        temp_status.innerHTML =
+          "<i>class='fas fa-cloud' style='color:#f1f2f6;'</i>";
+      }
+      //   datahide.classList.remove('data_hide')
+    } catch {
+      city_name.innerText = `type city name properly`;
+      //   datahide.classList.add('data_hide')
+    }
+  }
+};
+
+submitBtn.addEventListener("click", getInfo);
